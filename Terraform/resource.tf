2c0b2bba1a891f8bc6ee2203524d3b9ad66e9714
abhishekgowdrum <abhishekgowdru.m@gmail.com>
2026-08-14 14:47:27 +0530

    Update public_key path for aws_key_pair resource

--- Terraform/resource.tf
+++ Terraform/resource.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "SG1"{
 
 resource "aws_key_pair" "west2" {
   key_name   = "west2"
-  public_key = file("/var/lib/jenkins/west2.pem")
+  public_key = file("west2.pem")
 }
 
 resource "aws_instance" "EC2"{
